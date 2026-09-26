01387bc20bffb4970000e0a3bc137e659bb2f02b
paratysolar <luiz.zangrandi01@gmail.com>
2026-09-25 23:19:23 -0300

    feat(prop): photo-data index com 3 lotes

--- app/prop/photo-data.js
+++ app/prop/photo-data.js
@@ -1,3 +1,4 @@
 import { PHOTO_DATA_A } from './photo-data-a';
 import { PHOTO_DATA_B } from './photo-data-b';
-export const PHOTO_DATA = { ...PHOTO_DATA_A, ...PHOTO_DATA_B };
+import { PHOTO_DATA_C } from './photo-data-c';
+export const PHOTO_DATA = { ...PHOTO_DATA_A, ...PHOTO_DATA_B, ...PHOTO_DATA_C };
