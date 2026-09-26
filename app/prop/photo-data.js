0e95749e93340e115d4dffa51659947e58ee3bf3
paratysolar <luiz.zangrandi01@gmail.com>
2026-09-25 23:23:12 -0300

    feat(prop): index fotos 6 lotes

--- app/prop/photo-data.js
+++ app/prop/photo-data.js
@@ -1,4 +1,7 @@
-import { PHOTO_DATA_A } from './photo-data-a';
-import { PHOTO_DATA_B } from './photo-data-b';
-import { PHOTO_DATA_C } from './photo-data-c';
-export const PHOTO_DATA = { ...PHOTO_DATA_A, ...PHOTO_DATA_B, ...PHOTO_DATA_C };
+import { PHOTO_DATA_A1 } from './photo-data-a1';
+import { PHOTO_DATA_A2 } from './photo-data-a2';
+import { PHOTO_DATA_B1 } from './photo-data-b1';
+import { PHOTO_DATA_B2 } from './photo-data-b2';
+import { PHOTO_DATA_C1 } from './photo-data-c1';
+import { PHOTO_DATA_C2 } from './photo-data-c2';
+export const PHOTO_DATA = { ...PHOTO_DATA_A1, ...PHOTO_DATA_A2, ...PHOTO_DATA_B1, ...PHOTO_DATA_B2, ...PHOTO_DATA_C1, ...PHOTO_DATA_C2 };
